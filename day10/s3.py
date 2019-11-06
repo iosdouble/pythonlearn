6fd39df424450726e461fbc2c0720cb178e6d29d
nihui <18202504057@163.com>
2019-11-06 19:59:35 +0800

    编写字符串新s3

--- day10/s3.py
+++ day10/s3.py
@@ -1,3 +1,35 @@
-test = "nihui"
-v = test[0]
-print(v)
+# test = "nihui"
+# v = test[0]
+# print(v)
+
+
+# test = "nihui"
+# v = test[0:2]
+# print(v)
+
+# test = "nihui"
+# v = test[0:-1]
+# print(v)
+#
+# v1 = len(test)
+# print(v1)
+#
+# test1 = "倪辉"
+# v2 = len(test1)
+# print(v2)
+
+# test = "fafdahfakakdjf;ahfakjjladsads";
+# v = test.split("a")
+# print(v)
+
+
+
+# test = "操作一个高级操作"
+# index = 0;
+# while index<len(test):
+#     print(test[index])
+#     index +=1;
+
+# test = "这个操作很六"
+# for z in test:
+#     print(z)
