86fd24d8e616643a0898b014c826fe2bd234c337
nihui <18202504057@163.com>
2019-11-07 22:56:40 +0800

    编写字符串新s3

--- day10/s3.py
+++ day10/s3.py
@@ -33,3 +33,101 @@
 # test = "这个操作很六"
 # for z in test:
 #     print(z)
+
+
+
+
+
+# name = "nihui"
+# age = "18"
+#
+# info = name + age;
+# print(info)
+
+
+
+# test = 'nihuinihuinihui'
+#
+# v = test.replace("ih","bbb")
+#
+# print(v)
+#
+# v1 = test.replace("ih","AAA",1)
+# print(v1)
+
+
+# test = "这个是一个测试语句"
+#
+# for item in test:
+#     print(item)
+#     break;
+#
+#
+# for item in  test:
+#     continue
+#     print(item)
+
+#
+# v = range(100)
+# print(v)
+#
+# # for item in range(100):
+# #     print(item)
+#
+#
+#
+# for item in range(0,100,5):
+#     print(item)
+
+
+# 等待用户输入
+test = input(">>>")
+print(test)
+
+# 获取到用户长度
+l = len(test)
+print(l)
+
+# 打印字符
+r = range(0,l)
+for item in r:
+    print(item)
+    print(test[item])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day12/s1.py
@@ -0,0 +1,144 @@
+
+
+# list # 类，列表
+
+# li = [11,12,'nihui',[123,"age",[1,"A","B"]],"test"];
+
+# print(li[0])
+#
+# print(li[3:-1])
+#
+#
+# for item in li:
+#     print(item)
+#
+# i = 0;
+# while(i<len(li)):
+#     print(li[i])
+#     i+=1;
+
+
+# li = [11,12,'nihui',[123,"age",[1,"A","B"]],"test"];
+
+
+# c_li = [11,22,33,"123","nihui"]
+# s = ""
+#
+# for i in c_li:
+#     s = s + str(i)
+# print(s)
+#
+# s_li = ["123","1231","ninihiu"]
+#
+# v = "".join(s_li)
+#
+# print(v)
+
+
+
+
+# r = str(c_li)
+# print(r)
+
+
+
+
+
+
+# s = "wreqasdafdsfa"
+# new_li = list(s)
+# print(new_li)
+
+
+
+
+# v = 12 in li
+#
+# print(v)
+
+
+
+
+# del li[2:6]
+#
+# print(li)
+
+
+
+
+
+# li[1:3]=[123,1111]
+#
+# print(li)
+
+
+# li[1]=100;
+#
+# li[0]=["A","b",123]
+#
+# # 删除的第一种方式
+# del li[1]
+# print(li)
+
+
+
+# list
+
+# li = [11,55,22,44,77]
+#
+# li.sort()
+# print(li)
+#
+# li.sort(reverse=True)
+# print(li)
+
+# li.reverse()
+# print(li)
+#
+# li.remove(22)
+# print(li)
+
+# v = li.pop();
+# v1 = li.pop(1);
+# print(li)
+# print(v)
+# print(v1)
+
+# li.insert(0,99)
+# print(li)
+# v = li.index(33)
+# print(v)
+
+# li.extend([111,"nihui"])
+#
+# print(li)
+
+
+# v = li.count(22)
+# print(v)
+
+
+
+# v = li.copy()
+# print(v)
+
+
+
+
+# li.clear()
+# print(li)
+
+# v = li.append(5)
+#
+# print(v)
+#
+# print(li)
+
+
+
+v = 1
+v = 2
+print(v)
+
+
+
