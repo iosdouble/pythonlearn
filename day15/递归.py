9ba0b6077c1275285ac9d9516f32c1acc829a77d
iosdouble <18202504057@163.com>
2020-04-04 12:19:57 +0800

    提交函数是编程

--- day15/递归.py
+++ day15/递归.py
@@ -6,9 +6,32 @@
 # calc(10)
 
 
-def calc(n):
-    print(n)
-    if int(n/2) ==0:
-        return n
-    return calc(int(n/2))
-calc(10)
+# def calc(n):
+#     print(n)
+#     if int(n/2) ==0:
+#         return n
+#     return calc(int(n/2))
+# calc(10)
+
+
+person_list = ["test1","test2","test3","test4"]
+
+def ask_way(person_list):
+
+    if len(person_list)==0:
+        return '没人知道'
+
+    person = person_list.pop(0)
+
+    if person == 'test3':
+        return '%s 知道' %person
+
+    print('%s' %person + "不知道")
+
+    res = ask_way(person_list)
+
+    return res
+
+print(ask_way(person_list))
+
+
---
+++ demo001/作用域.py
@@ -0,0 +1,74 @@
+# name = "nihui"
+# def foo():
+#     name = "test"
+#     def bar():
+#         name = "admin"
+#         print(name)
+#     return bar
+#
+# a = foo()
+# print(a)
+# print(a())
+
+
+
+# def test1():
+#     print("in the test1")
+# def test():
+#     print("in the test")
+#     return test1
+#
+# res = test()
+# print(res())
+
+
+
+# name = "nihui"
+# def foo():
+#     name = "test"
+#     def bar():
+#         name = "admin"
+#         def tt():
+#             print(name)
+#         return tt
+#     return bar
+#
+# bar = foo();
+# print(bar)
+# print(bar())
+# print("=====")
+# tt = bar();
+# print(tt)
+# print(tt())
+#
+# foo()()()
+
+# def calc(x):
+#     return x+1
+# res = calc(10)
+# print(res)
+# print(calc)
+#
+# print(lambda x:x+1)
+#
+# func = lambda x:x+1
+#
+# print(func(10))
+
+
+
+
+# name = "nihui"
+# def change_name(x):
+#     return name+"_sg"
+# res = change_name(name)
+# print(res)
+#
+# print("================")
+#
+# func = lambda x:x+"_sg"
+# test = "test"
+# result = func(test)
+# print(result)
+
+
